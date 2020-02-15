5689d8985d39c8ea8dc38dab1dae589e989fb0cb
RogerJGomez <gomez.roger779@gmail.com>
2020-02-15 00:53:24 -0400

    first commit

---
+++ src/components/PokemonList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function PokemonList({ pokemons }) {
+    return (
+        <div>
+            {pokemons.map( pokemon => (
+                <div key = {pokemon}>{pokemon}</div>
+                ) 
+            )}
+        </div>
+    )
+}
